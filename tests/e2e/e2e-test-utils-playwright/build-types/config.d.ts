6e468c523e6abb0670a32267837b9253532f7dec
pavanpatil1 <=>
2022-11-14 15:45:46 +0530

    Fix the CI

--- tests/e2e/e2e-test-utils-playwright/build-types/config.d.ts
+++ tests/e2e/e2e-test-utils-playwright/build-types/config.d.ts
@@ -1,6 +1,6 @@
 declare const WP_ADMIN_USER: {
-    readonly username: "admin";
-    readonly password: "password";
+    readonly username: "automation";
+    readonly password: "automation";
 };
 declare const WP_USERNAME: string, WP_PASSWORD: string, WP_BASE_URL: string;
 export { WP_ADMIN_USER, WP_USERNAME, WP_PASSWORD, WP_BASE_URL };
