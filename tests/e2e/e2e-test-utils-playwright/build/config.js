95df5a2fe1f769076195260b8904f8fbcd3e82d7
alvitazwar <55917380+alvitazwar@users.noreply.github.com>
2022-12-09 01:28:27 +0600

    Implement Tesults

--- tests/e2e/e2e-test-utils-playwright/build/config.js
+++ tests/e2e/e2e-test-utils-playwright/build/config.js
@@ -2,11 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.WP_BASE_URL = exports.WP_PASSWORD = exports.WP_USERNAME = exports.WP_ADMIN_USER = void 0;
 const WP_ADMIN_USER = {
-    username: 'admin',
-    password: 'password',
+    username: 'automation',
+    password: 'automation',
 };
 exports.WP_ADMIN_USER = WP_ADMIN_USER;
-const { WP_USERNAME = WP_ADMIN_USER.username, WP_PASSWORD = WP_ADMIN_USER.password, WP_BASE_URL = 'http://alvitest.local/', } = process.env;
+const { WP_USERNAME = WP_ADMIN_USER.username, WP_PASSWORD = WP_ADMIN_USER.password, WP_BASE_URL = 'http://rtsocial.com', } = process.env;
 exports.WP_USERNAME = WP_USERNAME;
 exports.WP_PASSWORD = WP_PASSWORD;
 exports.WP_BASE_URL = WP_BASE_URL;
